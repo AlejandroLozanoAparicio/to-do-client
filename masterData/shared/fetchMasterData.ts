36f8cac4dd1867593e442df6a83b63aba8043f6b
alex <alex2001lozano@gmail.com>
2025-08-09 21:10:40 +0200

    feat: home changes

--- masterData/shared/fetchMasterData.ts
+++ masterData/shared/fetchMasterData.ts
@@ -27,26 +27,32 @@ const body = {
 export const fetchMasterData = async (): Promise<
   GetAllListsResponse | undefined
 > => {
-  const response = await fetch(url.href, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
+  try {
+    const response = await fetch(url.href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  if (!response.ok) {
-    // throw Error("[GET ALL LISTS] Service failed");
-    console.error("[GET ALL LISTS] Service failed");
+    if (!response.ok) {
+      // throw Error("[GET ALL LISTS] Service failed");
+      console.log("[GET ALL LISTS] Service failed");
+
+      return masterDataMock;
+    }
+
+    const data = (await response.json()) as ErrorResponse | GetAllListsResponse;
+
+    if (Object.hasOwn(data, "errors")) {
+      throw Error("[GET ALL LISTS] Errors in response");
+    }
+
+    return data as GetAllListsResponse;
+  } catch {
+    console.log("[GET ALL LISTS] Service failed");
 
     return masterDataMock;
   }
-
-  const data = (await response.json()) as ErrorResponse | GetAllListsResponse;
-
-  if (Object.hasOwn(data, "errors")) {
-    throw Error("[GET ALL LISTS] Errors in response");
-  }
-
-  return data as GetAllListsResponse;
 };
